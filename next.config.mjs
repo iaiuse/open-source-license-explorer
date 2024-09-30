e088660141ad4a52fd6c0cae2c690a43e2ce18af
richardson <chenyb@uyunsoft.cn>
2024-09-30 10:18:38 +0800

    修改配置，把config改好 了

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material', '@emotion/react', '@emotion/styled']
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+};
+
+export default nextConfig;
