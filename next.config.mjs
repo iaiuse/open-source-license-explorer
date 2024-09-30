8d55cf302d00fad47b700e6a60b7477ab72e564f
richardson <chenyb@uyunsoft.cn>
2024-09-30 10:03:34 +0800

    添加mui組件

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material', '@emotion/react', '@emotion/styled']
+  };
+  
+  module.exports = nextConfig;
