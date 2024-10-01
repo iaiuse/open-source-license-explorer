5cd29fb256b6a7d36537904985027fa16a5a5a7c
richardson <chenyb@uyunsoft.cn>
2024-10-01 20:39:05 +0800

    增加仓库代码的设置

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Link from 'next/link';
 
 const Header: React.FC = () => {
@@ -10,11 +11,21 @@ const Header: React.FC = () => {
           开源许可证探索器
         </Typography>
         <Link href="/" passHref>
-          <Button  component="a">首页</Button>
+          <Button component="a">首页</Button>
         </Link>
         <Link href="/About" passHref>
           <Button component="a">关于</Button>
         </Link>
+        {/* GitHub 链接按钮 */}
+        <IconButton
+          component="a"
+          href="https://github.com/iaiuse/open-source-license-explorer" // 将此链接替换为您的 GitHub 仓库链接
+          target="_blank"
+          rel="noopener noreferrer"
+          color="inherit"
+        >
+          <GitHubIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
